d8d01633654857a5aff41cb48eedc75ca370e2bb
Lyher99 <lyher750@gmail.com>
2025-01-16 23:02:50 +0700

    update

---
+++ resources/views/admin/page/auth/login.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Champa Admin</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+        <div class="container d-flex justify-content-center align-content-center" style="height: 100vh">
+            <div class=" m-auto rounded rounded-5 p-5 shadow">
+                <div class="">
+                    <div class="row d-flex justify-content-center p-4">
+                        <div class="col-5 col-lg-3">
+                            <img src="{{ asset('assets/img/icon/logo.png') }}" class="w-100" alt="">
+                        </div>
+                    </div>
+                    <h3 class="text-center fw-bold">Welcome to Champa Admin</h3>
+                    <div class="mb-3 py-4">
+                        <label for="" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="" id=""
+                            aria-describedby="emailHelpId" placeholder="abc@mail.com" />
+                        <label for="" class="form-label pt-4">Password</label>
+                        <input type="password" class="form-control" name="" id=""placeholder="" />
+
+                        <div class="w-100 d-flex justify-content-center py-4">
+                            <button class="btn btn-primary text-center px-4">Login</button>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
